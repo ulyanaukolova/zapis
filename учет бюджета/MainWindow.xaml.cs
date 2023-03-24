6bf99e1c8c1f535c34a50fd3e5dc760061b0dab6
ulyanaukolova <isip_u.a.ukolova@mpt.ru>
2023-03-24 22:57:18 +0300

    sos

--- учет бюджета/MainWindow.xaml.cs
+++ учет бюджета/MainWindow.xaml.cs
@@ -15,14 +15,37 @@ using System.Windows.Shapes;
 
 namespace учет_бюджета
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+     
+
         public MainWindow()
         {
             InitializeComponent();
+            Date.SelectedDate = DateTime.Today;
+
+            int sump = 0;
+            int summ = 0;
+            foreach (var entry in )
+            {
+                if (entry.IsIncome == false)
+                {
+                    summ += entry.Money;
+                }
+                else
+                {
+                    sump += entry.Money;
+                }
+
+              
+            }
         }
     }
+
+        private void Create_tip_Click(object sender, RoutedEventArgs e)
+        {
+            okno2 win = new okno2();
+            win.Show();
+        }
+    
 }

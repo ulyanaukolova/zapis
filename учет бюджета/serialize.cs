057a5a0923cdb4384700ebb38242387f698e33f3
ulyanaukolova <isip_u.a.ukolova@mpt.ru>
2023-06-21 13:45:40 +0300

    jddh

---
+++ учет бюджета/serialize.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace учет_бюджета
+{
+    internal class serialize
+    {
+        public static void Serialize<T>(T list)
+        {
+            string json = JsonConvert.SerializeObject(list);
+            string path = "";
+            File.WriteAllText(path, json);
+        }
+        public static T Deserialize<T>()
+        {
+            string path = "";
+            string json = File.ReadAllText(path); ;
+            T list = JsonConvert.DeserializeObject<T>(json);
+            return list;
+        }
+    }
+}
